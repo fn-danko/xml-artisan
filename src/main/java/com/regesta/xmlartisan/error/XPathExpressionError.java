1e4055d5c9524eef658f1535dd5f7dfb87305aff
Daniel Camarda <dtcamarda@regesta.com>
2026-03-15 10:01:23 +0100

    Remove initial classes to start from scratch

--- src/main/java/com/regesta/xmlartisan/error/XPathExpressionError.java
+++
@@ -1,7 +0,0 @@
-package net.fndanko.xmlartisan.error;
-
-public class XPathExpressionError extends RuntimeException {
-    public XPathExpressionError(String message, Throwable cause) {
-        super(message, cause);
-    }
-}
